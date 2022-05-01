075bd549172bcd7ef95320deecc83ac842e84411
ykkim0704 <102891899+ykkim0704@users.noreply.github.com>
2022-05-01 17:24:01 +0900

    Time: 291 ms (73.88%), Space: 49.5 MB (42.11%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.kt
+++ 2-add-two-numbers/2-add-two-numbers.kt
@@ -9,32 +9,33 @@
  */
 class Solution {
     fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
-        var listNode: ListNode? = null
+        val listNode: ListNode = ListNode(1)
 
         var list1 = l1
         var list2 = l2
         var addNum = 0
 
-        var tempNode: ListNode? = null
+        var tempNode: ListNode = listNode
         while(list1 != null || list2 != null) {
-            val sum = (list1?.`val` ?: 0) + (list2?.`val` ?: 0) + addNum
-            addNum = sum / 10
+            var sum = (list1?.`val` ?: 0) + (list2?.`val` ?: 0) + addNum
+            addNum = 0
 
-            if(tempNode == null) {
-                tempNode = ListNode(sum % 10)
-                listNode = tempNode
-            } else {
-                tempNode.next = ListNode(sum % 10)
-                tempNode = tempNode.next
+            if( sum >= 10) {
+                sum = sum.rem(10)
+                addNum = 1
             }
+
+            tempNode.next = ListNode(sum)
+            tempNode = tempNode.next
+
             list1 = list1?.next
             list2 = list2?.next
         }
-        
+
         if(addNum != 0) {
-            tempNode?.next = ListNode(addNum) 
+            tempNode?.next = ListNode(addNum)
         }
 
-        return listNode
+        return listNode.next
     }
 }
