47d3a7010cf4d4b1f05dd81b605b5729a58a1e4e
ykkim0704 <102891899+ykkim0704@users.noreply.github.com>
2022-05-14 14:41:38 +0900

    Time: 345 ms (12.76%), Space: 39.7 MB (30.34%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.kt
+++ 35-search-insert-position/35-search-insert-position.kt
@@ -1,11 +1,17 @@
 class Solution {
     fun searchInsert(nums: IntArray, target: Int): Int {
-        nums.forEachIndexed { index, i ->
-            if(i >= target) {
-               return index
+        var left = 0
+        var right = nums.size - 1
+        
+        while(left < right) {
+            val mid = (left + right) / 2
+            when {
+                nums[mid] == target -> return mid
+                nums[mid] > target -> right = mid -1
+                else -> left = mid + 1
             }
         }
 
-        return nums.size
+        return if(nums[left] < target) left + 1  else left
     }
 }
